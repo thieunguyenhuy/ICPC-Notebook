f4eda47b8e4010ac833fda893924c226c88138d7
thieunguyenhuy <huyxbox2609@gmail.com>
2025-11-06 16:54:04 +0700

    Fix some small mistakes

--- content/various/FastMod.h
+++ content/various/FastMod.h
@@ -1,4 +1,5 @@
 /**
+ * Author: KACTL
  * Source: https://en.wikipedia.org/wiki/Barrett_reduction
  * Description: Compute $a \% b$ about 5 times faster than usual, where $b$ is constant but not known at compile time.
  * Returns a value congruent to $a \pmod b$ in the range $[0, 2b)$.

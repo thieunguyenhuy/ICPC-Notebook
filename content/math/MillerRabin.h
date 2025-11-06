14f9e350d35a47f6ee644f6362302c5b3658f5e4
Thieu Nguyen Huy <huyxbox2609@gmail.com>
2025-11-06 12:24:33 +0700

    Add FFT and MillerRabin

---
+++ content/math/MillerRabin.h
@@ -0,0 +1,42 @@
+/**
+ * Author: Thieu Nguyen Huy
+ * Description: Primality check for numbers up to $7 \cdots 10^18$, extend bases for larger number
+ * Time: O(7 * logN)
+ * Status: Tested on judge.yosupo.jp
+ */
+
+namespace MillerRabin {
+    using T = unsigned long long;
+
+    T binpow(T x, T y, T mod) {
+        T ans = 1; x %= mod;
+        while (y > 0) {
+            if (y & 1) ans = (Int)ans * x % mod;
+            x = (Int)x * x % mod, y >>= 1;
+        }
+        return ans;
+    }
+
+    bool notPrime(T n, T base, T d, int s) {
+        if (base % n == 0) return false;
+        T x = binpow(base % n, d, n);
+        if (x == 1 || x == n - 1) return false;
+        for (int r = 1; r < s; ++r) {
+            x = (Int)x * x % n;
+            if (x == n - 1) return false;
+        }
+        return true;
+    }
+
+    bool isPrime(T n) {
+        if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+        int s = CTZ(n - 1); T d = n >> s;
+
+        const T bases[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+
+        for (T base : bases) {
+            if (notPrime(n, base, d, s)) return false;
+        }
+        return true;
+    }
+}
